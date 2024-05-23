d4e03fa12c2b9f5c0de877df5073d0e203f05d07
guilhermedreves <129562127+guilhermedreves@users.noreply.github.com>
2024-05-23 19:40:38 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,96 @@
+export const perguntas = [
+    {
+        enunciado: "O que é IA?",
+        alternativas: [
+            {
+                texto: "Inteligência Artificial",
+                afirmacao: [
+                    "Conhece sobre IA,"
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Inteligência Ambiental",
+                afirmacao: [
+                    "Precisa conhecer que IA é INTELIGÊNCIA ARTIFICIAL,"
+                ],
+                proxima: 3,
+            }
+        ]
+    },
+    {
+        enunciado: "IA (inteligência artificial) é um campo de aplicação e pesquisa da computação, que trata de meios (sistemas e programas) capazes de realizar tarefas antes só possíveis por humanos. De acordo com o enunciado, qual é a alternativa você considera correta?",
+        alternativas: [
+            {
+                texto: "A IA trouxe maior eficácia na tomada de decisões e na execução de tarefas",
+                afirmacao: [
+                    "todavia, a IA trouxe maior eficácia na tomada de decisões e na execução de tarefas,"
+                ],
+                proxima: 2,
+            },
+            {
+                texto: "A IA desenvoveu menos precisão e maior indice de erros humanos",
+                afirmacao: [
+                    "todavia, presencia que a IA pode prejudicar o indice de desenvolvimento humano,"
+                ],
+                proxima: 2,
+            }
+            
+        ]
+    },
+    {
+        enunciado: "Como que você acha que será o futuro da IA?",
+        alternativas: [
+            {
+                texto: "A IA irá dominar os seres humanos, onde os humanos ficaram sedentários e dependentes das máquinas, isso inclue também baixo nível de inteligência dos humanos",
+                afirmacao: [
+                    "contudo, imagina um futuro assustador, onde os humanos ficam sedentários e a IA dominante em tudo."
+                ],
+                proxima: 4,
+            },
+            {
+                texto: "A IA será um avanço grandioso, onde doenças sem curas serão possíveis curadas, aumento na saúde, avanços tecnologicos, os humanos serão uma raça bem avançada e evoluida",
+                afirmacao: [
+                    "contudo, vê um futuro que dá orgulho, onde temos grandes avanços tecnológicos e medicinais."
+                ],
+                proxima: 3,
+            }
+        ]
+    },
+    {
+        enunciado: "Qual é um exemplo de uso de IA no cotidiano?",
+        alternativas: [
+            {
+                texto: "Assistentes virtuais como Siri e Alexa",
+                afirmacao: [
+                    "Em definição, os assistentes virtuais são os mais presenntes na vida de hoje."
+                ],
+                proxima: 4,
+            },
+            {
+                texto: "Automação de carros",
+                afirmacao: [
+                    "Em definição, os carros possuem cada vez mais tecnologia inteligênte, podendo até estacionar sozinho?"
+                ],
+                proxima: 4,
+            }
+        ]
+    },
+    {
+        enunciado: "Sobre as Redes Neurais Artificiais?",
+        alternativas: [
+            {
+                texto: "inspiradas na estrutura e função do cérebro humano",
+            afirmacao: [
+                "Contendo, os avanços na medicina são incríveis, Elon Musk acabou de implementar um chipe em um humano, o que poderá vir? Telepatia? Controle total?"
+            ],
+            },
+            {
+                texto: "inspiradas no desenvolvimenmto dos sistemas sanguíneos do corpo ao cérebro",
+                afirmacao: [
+                    "Não foi a resposta correta, mas, os avanços na medicina são incríveis, Elon Musk acabou de implementar um chipe em um humano, o que poderá vir? Telepatia? Controle total?"
+                ],
+            }
+        ]
+    },
+];
